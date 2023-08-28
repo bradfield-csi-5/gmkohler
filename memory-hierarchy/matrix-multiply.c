93d04849ad33ce7824619babe3724518b794f9a6
Gregory M Kohler <gregorykohler@gmail.com>
2023-08-27 22:47:46 -0700

    tiling on matrix

--- memory-hierarchy/matrix-multiply.c
+++ memory-hierarchy/matrix-multiply.c
@@ -56,16 +56,40 @@ void matrix_multiply(double **C, double **A, double **B, int a_rows, int a_cols,
 }
 
 /**
- * First approach is to change the loop ordering so that we access B in
- * column-major order.  This improves speedup to 2.2-4.3x.
+ * * First approach is to change the loop ordering so that we access B in
+ *   column-major order.  This improves speedup to 2.2-4.3x.
+ *
+ * * Second approach is found from googling, "loop tiling".  The idea here
+ *   seems to be to reduce the ranges being accessed as to have them all in
+ *   cache memory at once.
+ *
+ *   This means we need to pick a tile size.  With 8-byte doubles, tile size
+ *   of 2 would let 8 * (2 * 2) = 32 bytes still fit in cache.  tile size of
+ *   3 would not fit in a cache.
+ *
+ *   However I'm seeing better results with tile_size = 8.  Not sure how to
+ *   explain this result.
+ *
+ *   This approach seems to raise average speedup to closer to 3x
  */
+#define TILE_SIZE 8
+#define min(a, b) (((a) < (b)) ? (a) : (b))
 void fast_matrix_multiply(double **C, double **A, double **B, int a_rows,
                           int a_cols, int b_cols) {
-  for (int i = 0; i < a_rows; i++) {
-    for (int k = 0; k < a_cols; k++) {
-      for (int j = 0; j < b_cols; j++) {
-        C[i][j] += A[i][k] * B[k][j];
-      }
-    }
-  }
+    for (int i = 0; i < a_rows; i+=TILE_SIZE) {
+        int t_row_stop = min(i + TILE_SIZE, a_rows);
+
+	    for(int dot = 0; dot < a_cols; dot++) {
+
+            for (int j = 0; j < b_cols; j+=TILE_SIZE) {
+                int t_col_stop = min(j + TILE_SIZE, b_cols);
+
+                for(int t_row = i; t_row < t_row_stop; t_row++) {
+                    for(int t_col = j; t_col < t_col_stop; t_col++) {
+                        C[t_row][t_col] += A[t_row][dot] * B[dot][t_col];
+                }
+            }
+        }
+		}
+	}
 }
