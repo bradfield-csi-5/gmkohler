febabc68cf6791f1ecc0f0551d6c487f16b2cc63
Gregory M Kohler <gregorykohler@gmail.com>
2023-10-19 20:38:31 -0700

    function sorting

--- compilers/overview/02-refactor/refactor.go
+++ compilers/overview/02-refactor/refactor.go
@@ -1,11 +1,12 @@
 package main
 
 import (
-	"log"
-	"os"
-
 	"github.com/dave/dst"
 	"github.com/dave/dst/decorator"
+	"log"
+	"os"
+	"slices"
+	"strings"
 )
 
 const src string = `package foo
@@ -30,8 +31,33 @@ func bar() {
 // Moves all top-level functions to the end, sorted in alphabetical order.
 // The "source file" is given as a string (rather than e.g. a filename).
 func SortFunctions(src string) (string, error) {
-	// TODO
-	return src, nil
+	parsed, err := decorator.Parse(src)
+
+	if err != nil {
+		return "", err
+	}
+	slices.SortStableFunc(parsed.Decls, func(a dst.Decl, b dst.Decl) int {
+		switch aType := a.(type) {
+		case *dst.FuncDecl:
+			// if both are funcs then sort by name
+			if bFunc, ok := b.(*dst.FuncDecl); ok {
+				return strings.Compare(aType.Name.Name, bFunc.Name.Name)
+			}
+			return 1 // otherwise A is a func and B is not so funcs go at bottom
+		default:
+			switch b.(type) {
+			case *dst.FuncDecl: // funcs go at bottom
+				return -1
+			default:
+				return 0
+			}
+		}
+	})
+	var buf strings.Builder
+	if err = decorator.Fprint(&buf, parsed); err != nil {
+		return "", err
+	}
+	return buf.String(), nil
 }
 
 func main() {
