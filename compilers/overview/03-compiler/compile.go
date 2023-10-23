8444853ad1a5739b9d790c773a4c5a527e391b9a
Gregory M Kohler <gregorykohler@gmail.com>
2023-10-22 22:06:46 -0700

    Refactor to statement/expr setup with stored vars
    Basing this off the solution but this was pretty much needed with
    the way my code was going.  I like the delineation which is where
    my code was getting messy.

--- compilers/overview/03-compiler/compile.go
+++ compilers/overview/03-compiler/compile.go
@@ -1,12 +1,11 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"go/ast"
 	"go/token"
-	"slices"
 	"strconv"
-	"strings"
 )
 
 // Given an AST node corresponding to a function (guaranteed to be
@@ -17,82 +16,99 @@ import (
 // be read from memory addresses `1` and `2`, and the return value
 // should be written to memory address `0`.
 func compile(node *ast.FuncDecl) (string, error) {
-	var buf strings.Builder
-	paramNames := node.Type.Params.List[0].Names
-	params := make([]string, len(paramNames))
-	for j, p := range paramNames {
-		params[j] = p.Name
-	}
-	for _, s := range node.Body.List {
-		switch st := s.(type) {
-		case *ast.ReturnStmt:
-			res := st.Results[0]
-			switch rt := res.(type) {
-			case *ast.BasicLit:
-				intVal, err := strconv.Atoi(rt.Value)
-				if err != nil {
-					return "", err
-				}
-				retVal := byte(intVal)
-				buf.WriteString(fmt.Sprintf("pushi %d\n", retVal))
-			case *ast.BinaryExpr:
-				xStatement, err := handleParam(rt.X, params)
-				if err != nil {
-					return "", err
-				}
-				buf.WriteString(xStatement)
-				yStatement, err := handleParam(rt.Y, params)
-				if err != nil {
-					return "", err
-				}
-				buf.WriteString(yStatement)
-				switch rt.Op {
-				case token.ADD:
-					buf.WriteString("add\n")
-				case token.SUB:
-					buf.WriteString("sub\n")
-				case token.MUL:
-					buf.WriteString("mul\n")
-				case token.QUO:
-					buf.WriteString("div\n")
-				default:
-					return "", fmt.Errorf("unsupported token %s", rt.Op.String())
-				}
-			default:
-				return "", fmt.Errorf(
-					"unsupported return type %T (%+v)",
-					rt,
-					rt,
-				)
-			}
-			buf.WriteString("pop 0\n")
-		default:
-			return "", fmt.Errorf("unsupported statement type %T (%+v)", st, st)
+	var buf bytes.Buffer
+	var nextLoc byte = 1
+	params := make(map[string]byte)
+	for _, p := range node.Type.Params.List {
+		for _, n := range p.Names {
+			params[n.Name] = nextLoc
+			nextLoc++
 		}
 	}
-	fmt.Printf("%T", node)
-	buf.WriteString("halt\n")
+	s := scope{identifiers: params}
+	err := s.compileStatement(node.Body, &buf)
+	if err != nil {
+		return "", err
+	}
+
 	return buf.String(), nil
 }
-func handleParam(ident ast.Expr, params []string) (string, error) {
-	switch xt := ident.(type) {
+
+type scope struct {
+	identifiers map[string]byte
+}
+
+func (s *scope) compileExpression(e ast.Expr, buf *bytes.Buffer) error {
+	var err error
+	switch et := e.(type) {
 	case *ast.BasicLit:
-		fmt.Printf("found type %T", xt)
-		v, err := strconv.Atoi(xt.Value)
+		intVal, err := strconv.Atoi(et.Value)
 		if err != nil {
-			return "", err
+			return err
 		}
-		return fmt.Sprintf("pushi %d\n", v), nil
+		buf.WriteString(fmt.Sprintf("pushi %d\n", byte(intVal)))
+		return nil
+	case *ast.BinaryExpr:
+		err = s.compileExpression(et.X, buf)
+		if err != nil {
+			return err
+		}
+		err = s.compileExpression(et.Y, buf)
+		if err != nil {
+			return err
+		}
+		switch et.Op {
+		case token.ADD:
+			buf.WriteString("add\n")
+		case token.SUB:
+			buf.WriteString("sub\n")
+		case token.MUL:
+			buf.WriteString("mul\n")
+		case token.QUO:
+			buf.WriteString("div\n")
+		default:
+			return fmt.Errorf("unsupported token %s", et.Op.String())
+		}
+		return nil
 	case *ast.Ident:
-		j := slices.Index(params, xt.Name)
-		if j < 0 {
-			return "", fmt.Errorf(
-				"found identifier %s not in parameters", xt.Name)
+		loc, ok := s.identifiers[et.Name]
+		if !ok {
+			return fmt.Errorf("unrecognized identifier %s", et.Name)
 		}
-		// params are 1-indexed
-		return fmt.Sprintf("push %d\n", j+1), nil
+		buf.WriteString(fmt.Sprintf("push %d\n", loc))
+		return nil
+	case *ast.ParenExpr:
+		err = s.compileExpression(et.X, buf)
+		if err != nil {
+			return err
+		}
+		return nil
 	default:
-		fmt.Printf("found type %T", xt)
-		return "", nil
+		return fmt.Errorf("unrecognized expression %T", e)
+	}
+}
+
+func (s *scope) compileStatement(stmt ast.Stmt, buf *bytes.Buffer) error {
+	var err error
+	switch stmtType := stmt.(type) {
+	case *ast.BlockStmt:
+		for _, subStmt := range stmtType.List {
+			err = s.compileStatement(subStmt, buf)
+			if err != nil {
+				return err
+			}
+		}
+		return nil
+	case *ast.ReturnStmt:
+		res := stmtType.Results[0]
+		err := s.compileExpression(res, buf)
+		if err != nil {
+			return err
+		}
+		buf.WriteString("pop 0\n")
+		buf.WriteString("halt\n")
+		return nil
+	default:
+		return fmt.Errorf("unsupported statement type %T", stmtType)
 	}
 }
