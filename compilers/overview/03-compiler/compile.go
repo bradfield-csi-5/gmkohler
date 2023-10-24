77b2ebcd897672793431c0dfd658f403b5fba875
Gregory M Kohler <gregorykohler@gmail.com>
2023-10-23 18:04:58 -0700

    declarations/for-loops
    I looked at the solution for these ones but makes sense

--- compilers/overview/03-compiler/compile.go
+++ compilers/overview/03-compiler/compile.go
@@ -8,6 +8,12 @@ import (
 	"strconv"
 )
 
+const (
+	templateJeqz  = "jeqz %s\n"
+	templateJump  = "jump %s\n"
+	templateLabel = "label %s\n"
+)
+
 // Given an AST node corresponding to a function (guaranteed to be
 // of the form `func f(x, y byte) byte`), compile it into assembly
 // code.
@@ -17,15 +23,18 @@ import (
 // should be written to memory address `0`.
 func compile(node *ast.FuncDecl) (string, error) {
 	var buf bytes.Buffer
-	var nextLoc byte = 1
-	params := make(map[string]byte)
+	var nextLoc addr = 1
+	params := make(map[string]addr)
 	for _, p := range node.Type.Params.List {
 		for _, n := range p.Names {
 			params[n.Name] = nextLoc
 			nextLoc++
 		}
 	}
-	s := scope{identifiers: params}
+	s := scope{
+		addrsByVarName:    params,
+		nextAvailableAddr: nextLoc,
+	}
 	err := s.compileStatement(node.Body, &buf)
 	if err != nil {
 		return "", err
@@ -34,8 +43,19 @@ func compile(node *ast.FuncDecl) (string, error) {
 	return buf.String(), nil
 }
 
+type addr byte
 type scope struct {
-	identifiers map[string]byte
+	addrsByVarName    map[string]addr
+	nextAvailableAddr addr
+}
+
+func (s *scope) declareVar(name string) error {
+	if _, exists := s.addrsByVarName[name]; exists {
+		return fmt.Errorf("variable %s already exists", name)
+	}
+	s.addrsByVarName[name] = s.nextAvailableAddr
+	s.nextAvailableAddr++
+	return nil
 }
 
 func (s *scope) compileExpression(e ast.Expr, buf *bytes.Buffer) error {
@@ -83,7 +103,7 @@ func (s *scope) compileExpression(e ast.Expr, buf *bytes.Buffer) error {
 		}
 		return nil
 	case *ast.Ident:
-		loc, ok := s.identifiers[et.Name]
+		loc, ok := s.addrsByVarName[et.Name]
 		if !ok {
 			return fmt.Errorf("unrecognized identifier %s", et.Name)
 		}
@@ -112,7 +132,7 @@ func (s *scope) compileStatement(stmt ast.Stmt, buf *bytes.Buffer) error {
 				sType.Lhs[0],
 			)
 		}
-		lhsAddr, ok := s.identifiers[lhsId.Name]
+		lhsAddr, ok := s.addrsByVarName[lhsId.Name]
 		if !ok {
 			return fmt.Errorf("unrecognized identifier %s", lhsId.Name)
 		}
@@ -134,6 +154,47 @@ func (s *scope) compileStatement(stmt ast.Stmt, buf *bytes.Buffer) error {
 			}
 		}
 		return nil
+	case *ast.DeclStmt:
+		genDecl, ok := sType.Decl.(*ast.GenDecl)
+		if !ok {
+			return fmt.Errorf(
+				"only general declarations are supported.  received %T",
+				sType,
+			)
+		}
+		valueSpec, ok := genDecl.Specs[0].(*ast.ValueSpec)
+		if !ok {
+			return fmt.Errorf(
+				"only val declarations are supported.  received %T",
+				genDecl.Specs[0],
+			)
+		}
+		err = s.declareVar(valueSpec.Names[0].Name)
+		if err != nil {
+			return err
+		}
+		return nil
+	case *ast.ForStmt:
+		// does not handle init/increment
+		labelBefore := fmt.Sprintf("for-%d-before", sType.For)
+		labelAfter := fmt.Sprintf("for-%d-after", sType.For)
+		// we'll jump back here based on condition
+		buf.WriteString(fmt.Sprintf(templateLabel, labelBefore))
+		err = s.compileExpression(sType.Cond, buf)
+		if err != nil {
+			return err
+		}
+		// skip body if condition is false
+		buf.WriteString(fmt.Sprintf(templateJeqz, labelAfter))
+		// evaluate body
+		err = s.compileStatement(sType.Body, buf)
+		if err != nil {
+			return err
+		}
+		buf.WriteString(fmt.Sprintf(templateJump, labelBefore))
+		buf.WriteString(fmt.Sprintf(templateLabel, labelAfter))
+
+		return nil
 	case *ast.IfStmt:
 		// requires labels for branching
 		labelElse := fmt.Sprintf("if-%d-else", sType.If)
@@ -149,7 +210,7 @@ func (s *scope) compileStatement(stmt ast.Stmt, buf *bytes.Buffer) error {
 		}
 		// we either jump to the else or after in the false case; otherwise
 		// we proceed with the body:
-		buf.WriteString(fmt.Sprintf("jeqz %s\n", jumpLabel))
+		buf.WriteString(fmt.Sprintf(templateJeqz, jumpLabel))
 		err = s.compileStatement(sType.Body, buf)
 		if err != nil {
 			return err
@@ -157,15 +218,14 @@ func (s *scope) compileStatement(stmt ast.Stmt, buf *bytes.Buffer) error {
 		if sType.Else != nil {
 			// if we have to write the else statement, jump over it in the
 			// body statement that we just wrote
-			buf.WriteString(fmt.Sprintf("jump %s\n", labelAfter))
-			buf.WriteString(fmt.Sprintf("label %s\n", labelElse))
+			buf.WriteString(fmt.Sprintf(templateJump, labelAfter))
+			buf.WriteString(fmt.Sprintf(templateLabel, labelElse))
 			err = s.compileStatement(sType.Else, buf)
 			if err != nil {
 				return err
 			}
 		}
-		buf.WriteString(fmt.Sprintf("label %s\n", labelAfter))
-		//err = s.compileExpression(sType.Cond.)
+		buf.WriteString(fmt.Sprintf(templateLabel, labelAfter))
 		return nil
 	case *ast.ReturnStmt:
 		res := sType.Results[0]
